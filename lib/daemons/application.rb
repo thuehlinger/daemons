thuehlinger/daemons

commit 70f69a71ce1f1e19e254203d6b8bcff5abc3796c

* start_proc: Write out the PID file in the newly created proc to avoid race conditions. * daemonize.rb: remove to simplify licensing (replaced by daemonization.rb).

lib/daemons/application.rb
@@ -78,9 +78,9 @@ module Daemons
     # this function is only used to daemonize the currently running process (Daemons.daemonize)
     def start_none
       unless options[:ontop]
-        Daemonize.daemonize(output_logfile, @group.app_name)
+        Daemonization.daemonize(output_logfile, @group.app_name)
       else
-        Daemonize.simulate(output_logfile)
+        Daemonization.simulate(output_logfile)
       end
       
       @pid.pid = Process.pid
@@ -125,9 +125,9 @@ module Daemons
       end
       
       unless options[:ontop]
-        Daemonize.daemonize(output_logfile, @group.app_name)
+        Daemonization.daemonize(output_logfile, @group.app_name)
       else
-        Daemonize.simulate(output_logfile)
+        Daemonization.simulate(output_logfile)
       end
       
       # note that we cannot remove the pid file if we run in :ontop mode (i.e. 'ruby ctrl_exec.rb run')
@@ -142,9 +142,9 @@ module Daemons
     
     def start_load
       unless options[:ontop]
-        Daemonize.daemonize(output_logfile, @group.app_name)
+        Daemonization.daemonize(output_logfile, @group.app_name)
       else
-        Daemonize.simulate(output_logfile)
+        Daemonization.simulate(output_logfile)
       end
       
       @pid.pid = Process.pid
@@ -206,6 +206,9 @@ module Daemons
       return unless p = options[:proc]
     
       myproc = proc do 
+        
+        @pid.pid = Process.pid
+        
         # We need this to remove the pid-file if the applications exits by itself.
         # Note that <tt>at_text</tt> will only be run if the applications exits by calling 
         # <tt>exit</tt>, and not if it calls <tt>exit!</tt> (so please don't call <tt>exit!</tt>
@@ -246,16 +249,16 @@ module Daemons
           end
         }
         
+        started()
+        
         p.call()
       end
       
       unless options[:ontop]
-        @pid.pid = Daemonize.call_as_daemon(myproc, output_logfile, @group.app_name)
+        Daemonization.call_as_daemon(myproc, output_logfile, @group.app_name)
         
       else
-        Daemonize.simulate(output_logfile)
-        
-        @pid.pid = Process.pid
+        Daemonization.simulate(output_logfile)
         
         myproc.call
         
@@ -265,7 +268,7 @@ module Daemons
 # why did we use the code below??
         # unless pid = Process.fork
         #   @pid.pid = pid
-        #   Daemonize.simulate(logfile)
+        #   Daemonization.simulate(logfile)
         #   options[:proc].call
         #   exit
         # else
@@ -273,7 +276,6 @@ module Daemons
         # end
       end
       
-      started()
     end
     
     

LICENSE
@@ -20,10 +20,3 @@ HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
 WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 OTHER DEALINGS IN THE SOFTWARE.
-
-This license does not apply to daemonize.rb, which is was written by 
-Travis Whitton und published under the following license:
-
-The Daemonize extension module is copywrited free software by Travis Whitton
-<whitton@atlantic.net>. You can redistribute it under the terms specified in
-the COPYING file of the Ruby distribution.

README
@@ -1,4 +1,4 @@
-= Daemons Version 1.1.6
+= Daemons Version 1.1.7
 
 (See Releases for release-specific information)
 
@@ -13,9 +13,6 @@ these processes from the main application.
 Besides this basic functionality, daemons offers many advanced features like <i>exception backtracing</i> 
 and logging (in case your ruby script crashes) and <i>monitoring</i> and automatic restarting of your processes
 if they crash.
-
-Daemons includes the <tt>daemonize.rb</tt> script written by <i>Travis Whitton</i> to do the daemonization
-process.
 
 == Basic Usage
 
@@ -211,13 +208,6 @@ HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
 WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 OTHER DEALINGS IN THE SOFTWARE.
-
-This license does not apply to daemonize.rb, which is was written by 
-Travis Whitton und published under the following license:
-
-The Daemonize extension module is copywrited free software by Travis Whitton
-<whitton@atlantic.net>. You can redistribute it under the terms specified in
-the COPYING file of the Ruby distribution.
 
 == Feedback and other resources
 

Releases
@@ -1,5 +1,10 @@
 = Daemons Release History
 
+== Release 1.1.7: February 6, 2012
+
+* start_proc: Write out the PID file in the newly created proc to avoid race conditions.
+* daemonize.rb: remove to simplify licensing (replaced by daemonization.rb).
+
 == Release 1.1.6: January 18, 2012
 
 * Add the :app_name option for the "call" daemonization mode.

daemons.gemspec
@@ -4,8 +4,8 @@ Gem::Specification.new do |s|
   s.name = %q{daemons}
   
   
-  s.version = "1.1.6"
-  s.date = %q{2012-01-17}
+  s.version = "1.1.7"
+  s.date = %q{2012-01-29}
   
   
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=

examples/run/ctrl_proc.rb.output
@@ -106,3 +106,10 @@ ping from proc!
 ping from proc!
 ping from proc!
 ping from proc!
+ctrl_proc.rb: process with pid 3187 started.
+ping from proc!
+ping from proc!
+ping from proc!
+ctrl_proc.rb: process with pid 1659 started.
+ping from proc!
+ping from proc!

lib/daemons.rb
@@ -66,9 +66,9 @@ require 'timeout'
 #
 module Daemons
 
-  VERSION = "1.1.6"
+  VERSION = "1.1.7"
   
-  require 'daemons/daemonize'
+  require 'daemons/daemonization'
   
   
   # Passes control to Daemons.

lib/daemons/daemonization.rb
@@ -1,0 +1,166 @@
+module Daemonization
+  
+  # Try to fork if at all possible retrying every 5 sec if the
+  # maximum process limit for the system has been reached
+  def safe_fork
+    tryagain = true
+
+    while tryagain
+      tryagain = false
+      begin
+        if pid = fork
+          return pid
+        end
+      rescue Errno::EWOULDBLOCK
+        sleep 5
+        tryagain = true
+      end
+    end
+  end
+  module_function :safe_fork
+  
+  
+  # Simulate the daemonization process (:ontop mode)
+  # NOTE: STDOUT and STDERR will not be redirected to the logfile, 
+  # because in :ontop mode, we normally want to see the output
+  def simulate(logfile_name = nil)
+    # Release old working directory
+    Dir.chdir "/"   
+
+    close_io()
+
+    # Free STDIN and point them somewhere sensible
+    begin; STDIN.reopen "/dev/null"; rescue ::Exception; end       
+  end
+  module_function :simulate
+  
+  
+  # Call a given block as a daemon
+  def call_as_daemon(block, logfile_name = nil, app_name = nil)
+    # we use a pipe to return the PID of the daemon
+    rd, wr = IO.pipe
+    
+    if tmppid = safe_fork
+      # in the parent
+      
+      wr.close
+      pid = rd.read.to_i
+      rd.close
+      
+      Process.waitpid(tmppid)
+      
+      return pid
+    else
+      # in the child
+      
+      rd.close
+      
+      # Detach from the controlling terminal
+      unless sess_id = Process.setsid
+        raise Daemons.RuntimeException.new('cannot detach from controlling terminal')
+      end
+  
+      # Prevent the possibility of acquiring a controlling terminal
+      trap 'SIGHUP', 'IGNORE'
+      exit if pid = safe_fork
+  
+      wr.write Process.pid
+      wr.close
+      
+      $0 = app_name if app_name
+      
+      # Release old working directory
+      Dir.chdir "/"   
+  
+      close_io()
+
+      redirect_io(logfile_name)  
+    
+      block.call
+      
+      exit
+    end
+  end
+  module_function :call_as_daemon
+  
+  
+  # Transform the current process into a daemon
+  def daemonize(logfile_name = nil, app_name = nil)
+    # Split rand streams between spawning and daemonized process
+    srand 
+    
+     # Fork and exit from the parent
+    safe_fork and exit
+
+    # Detach from the controlling terminal
+    unless sess_id = Process.setsid
+      raise Daemons.RuntimeException.new('cannot detach from controlling terminal')
+    end
+
+    # Prevent the possibility of acquiring a controlling terminal
+    trap 'SIGHUP', 'IGNORE'
+    exit if pid = safe_fork
+    
+    $0 = app_name if app_name
+    
+    # Release old working directory
+    Dir.chdir "/"  
+
+    close_io()
+
+    redirect_io(logfile_name)
+    
+    return sess_id
+  end
+  module_function :daemonize
+  
+  
+  def close_io()
+    # Make sure all input/output streams are closed
+    # Part I: close all IO objects (except for STDIN/STDOUT/STDERR)
+    ObjectSpace.each_object(IO) do |io|
+      unless [STDIN, STDOUT, STDERR].include?(io)
+        begin
+          unless io.closed?
+            io.close
+          end
+        rescue ::Exception
+        end
+      end
+    end
+    
+    # Make sure all input/output streams are closed
+    # Part II: close all file decriptors (except for STDIN/STDOUT/STDERR)
+    ios = Array.new(8192) {|i| IO.for_fd(i) rescue nil}.compact
+    ios.each do |io|
+      next if io.fileno < 3
+      io.close
+    end
+  end
+  module_function :close_io
+  
+  
+  # Free STDIN/STDOUT/STDERR file descriptors and
+  # point them somewhere sensible
+  def redirect_io(logfile_name)
+    begin; STDIN.reopen "/dev/null"; rescue ::Exception; end       
+     
+    if logfile_name
+      begin
+        STDOUT.reopen logfile_name, "a" 
+        File.chmod(0644, logfile_name)
+        STDOUT.sync = true
+      rescue ::Exception
+        begin; STDOUT.reopen "/dev/null"; rescue ::Exception; end
+      end
+    else
+      begin; STDOUT.reopen "/dev/null"; rescue ::Exception; end
+    end
+    
+    begin; STDERR.reopen STDOUT; rescue ::Exception; end
+    STDERR.sync = true
+  end
+  module_function :redirect_io
+  
+  
+end

commit 6da4cd09c0919ec8b0307ead3f149aa31c85f4c5

fixes issue #22 introduced by 70f69a7 which doesn't asign pid when running Daemons.call

lib/daemons/application.rb
@@ -208,8 +208,6 @@ module Daemons
 
       myproc = proc do
 
-        @pid.pid = Process.pid
-
         # We need this to remove the pid-file if the applications exits by itself.
         # Note that <tt>at_text</tt> will only be run if the applications exits by calling
         # <tt>exit</tt>, and not if it calls <tt>exit!</tt> (so please don't call <tt>exit!</tt>
@@ -249,14 +247,11 @@ module Daemons
             exit
           end
         end
-
-        started
-
         p.call
       end
 
       unless options[:ontop]
-        Daemonize.call_as_daemon(myproc, output_logfile, @group.app_name)
+        @pid.pid = Daemonize.call_as_daemon(myproc, output_logfile, @group.app_name)
 
       else
         Daemonize.simulate(output_logfile)
@@ -276,6 +271,7 @@ module Daemons
         #   Process.detach(@pid.pid)
         # end
       end
+      started
     end
 
     def start(restart = false)