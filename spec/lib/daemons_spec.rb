ec9939ae4417c768870567aa24c80c3f78ff77ff
Adam Cuppy <adam@codingzeal.com>
2015-04-27 21:31:29 -0700

    Add Daemon.run spec
    Current the specs are stubbing out most of the interaction.  It's expected that we test that functionality via an isolated test on each related unit.

--- spec/lib/daemons_spec.rb
+++ spec/lib/daemons_spec.rb
@@ -1,20 +1,62 @@
 require 'spec_helper'
 
 describe Daemons do
+  let(:script) {
+    'spec/support/test_script.rb'
+  }
+  let(:shell_args) {
+    ['run']
+  }
+  let(:options) {
+    { ARGV: shell_args }
+  }
+
   describe '.run' do
-
-    subject { described_class.run script, options }
-
-    let(:script) {
-      'spec/support/test_script.rb'
+    let(:group) {
+      instance_double 'ApplicationGroup'
     }
-    let(:shell_args) {
-      ['run']
+    let(:controller) {
+      instance_double 'Controller', group: group, catch_exceptions: ->{}, run: ->{}
     }
-    let(:options) {
-      { ARGV: shell_args }
+    let(:ctrl_instance) {
+      described_class.instance_variable_get :@controller
+    }
+    let(:group_instance) {
+      described_class.instance_variable_get :@group
     }
 
-    it { expect(subject).to be_a Daemons::ApplicationGroup }
+    before do
+      allow(Daemons::Controller)
+        .to receive(:new)
+        .and_return controller
+
+      allow(controller)
+        .to receive(:catch_exceptions)
+        .and_yield
+
+      Daemons.run script, options
+    end
+
+
+    it { expect(ctrl_instance).to eq controller }
+    it { expect(group_instance).to eq group }
+    it { expect(controller).to have_received :catch_exceptions }
+    it { expect(controller).to have_received :run }
+
+    context 'with custom options hash' do
+      let(:options) {
+        { foo: "bar", ARGV: shell_args }
+      }
+
+      it 'passes a custom options had to the controller' do
+        expect(Daemons::Controller)
+          .to have_received(:new)
+          .with(options, shell_args)
+      end
+    end
+
+    context 'when options does NOT include :ARGV' do
+      pending
+    end
   end
 end
