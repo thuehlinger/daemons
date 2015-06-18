894156de55c593e2ac3aacab724ce3d4a0d0ff75
Adam Cuppy <adam@codingzeal.com>
2015-06-18 11:23:06 -0700

    Add coverage for Daemons.run_proc

--- spec/lib/daemons_spec.rb
+++ spec/lib/daemons_spec.rb
@@ -10,14 +10,27 @@ describe Daemons do
   let(:options) {
     { ARGV: shell_args }
   }
+  let(:group) {
+    instance_double 'ApplicationGroup'
+  }
+  let(:controller) {
+    instance_double 'Controller',
+      group: group,
+      catch_exceptions: ->{},
+      run: ->{}
+  }
+
+  def stub_controller
+    allow(Daemons::Controller)
+      .to receive(:new)
+      .and_return controller
+
+    allow(controller)
+      .to receive(:catch_exceptions)
+      .and_yield
+  end
 
   describe '.run' do
-    let(:group) {
-      instance_double 'ApplicationGroup'
-    }
-    let(:controller) {
-      instance_double 'Controller', group: group, catch_exceptions: ->{}, run: ->{}
-    }
     let(:ctrl_instance) {
       described_class.instance_variable_get :@controller
     }
@@ -26,17 +39,9 @@ describe Daemons do
     }
 
     before do
-      allow(Daemons::Controller)
-        .to receive(:new)
-        .and_return controller
-
-      allow(controller)
-        .to receive(:catch_exceptions)
-        .and_yield
-
+      stub_controller
       Daemons.run script, options
     end
-
 
     it { expect(ctrl_instance).to eq controller }
     it { expect(group_instance).to eq group }
@@ -54,9 +59,49 @@ describe Daemons do
           .with(options, shell_args)
       end
     end
+  end
 
-    context 'when options does NOT include :ARGV' do
-      pending
+  describe '.run_proc' do
+    let(:app_name) { 'my_app' }
+    let(:block) { ->{} }
+    let(:test_object) { double }
+    let(:dir_mode) { :normal }
+    let(:dir) {
+      File.expand_path '.'
+    }
+    let(:routine_block) {
+      ->{ test_object.run }
+    }
+    let(:options) {
+      { ARGV: shell_args,
+        app_name: app_name,
+        mode: :proc,
+        proc: routine_block,
+        dir_mode: dir_mode,
+        dir: dir }
+    }
+
+    context 'when options[:dir_mode] is nil' do
+      let(:dir_mode) { nil }
+
+      before do
+        allow(test_object)
+          .to receive(:run)
+
+        stub_controller
+        Daemons.run_proc(app_name, options, &routine_block)
+      end
+
+      it do
+        expect(Daemons::Controller)
+          .to have_received(:new)
+          .with options, shell_args
+      end
+
+      it do
+        expect(controller)
+          .to have_received(:run)
+      end
     end
   end
 end
